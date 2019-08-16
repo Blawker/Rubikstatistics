53c45d5448411131309a3ae5965435ff94ef5bdd
Marion Séminel <marionement@gmail.com>
2019-08-16 12:42:42 +0200

    add Param and Cube Modal

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -1,14 +1,34 @@
-
+/** ADD MODAL EVENT HANDLER**/
 const addBtn = document.getElementById('addBtn');
 const addModal = document.getElementById('addModal');
 
-const openModal = () => {
-  addModal.style.display = 'block';
-}
+const openModal = (element) => {
+  element.style.display = 'block';
+};
 
-addBtn.addEventListener('click', () => openModal());
+addBtn.addEventListener('click', () => openModal(addModal));
 addModal.addEventListener('click', (e) => {
   if (e.target.id === 'addModal') {
     addModal.style.display = 'none';
+  }
+})
+
+/** PARAMETERS MODAL EVENT HANDLER**/
+const openParamModal = document.getElementById('openParamModal');
+const paramModal = document.getElementById('paramModal');
+openParamModal.addEventListener('click', () => openModal(paramModal));
+paramModal.addEventListener('click', (e) => {
+  if (e.target.id === 'paramModal') {
+    paramModal.style.display = 'none';
+  }
+})
+
+/** CUBE MODAL EVENT HANDLER **/
+const cube = document.getElementById('activeCube');
+const cubeModal = document.getElementById('cubeModal');
+activeCube.addEventListener('click', () => openModal(cubeModal));
+cubeModal.addEventListener('click', (e) => {
+  if (e.target.id === 'cubeModal') {
+    cubeModal.style.display = 'none';
   }
 })
